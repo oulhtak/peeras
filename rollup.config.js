40087789f10fd2f37e78266fef672fd31bac71c0
47kyo <68kenichi@gmail.com>
2024-01-01 15:59:23 +0100

    remove dist

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,6 @@ module.exports = {
   input: "src/index.js",
   output: [
     {
-      file: path.resolve(__dirname, "dist/peeras_bundle.js"),
-      format: "es",
-    },
-    {
       file: path.resolve(__dirname, "example/lib/peeras_bundle.js"),
       format: "es",
     },
